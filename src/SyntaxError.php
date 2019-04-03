11322039ee23446bc83a289319031d42c9d8cf1d
Tim Starling <tstarling@wikimedia.org>
2019-04-03 16:41:11 +1100

    SyntaxError: correctly call parent constructor
    Change-Id: Id077cf032469118c2f74adc0a179348552c982fa

--- src/SyntaxError.php
+++ src/SyntaxError.php
@@ -3,14 +3,18 @@
 namespace WikiPEG;
 
 class SyntaxError extends \Exception {
-  public $message, $expected, $found, $location, $name;
+  public $expected, $found, $location, $name;
 
   public function __construct($message, $expected, $found, $location) {
-    $this->message  = $message;
+    parent::__construct( $message );
     $this->expected = $expected;
     $this->found    = $found;
     $this->location = $location;
 
     $this->name     = "SyntaxError";
+  }
+
+  public function getLocationString() {
+    return $this->location['start']['line'] . ':' . $this->location['start']['column'];
   }
 }
