8a381fe300c32c2063ab62259714835a509068c7
James D. Forrester <jforrester@wikimedia.org>
2023-04-17 16:37:50 -0400

    build: Switch phan to special library mode
    Bug: T334492
    Change-Id: I4a0aa6fd5b6db2780e65849632b3158bddfc7ce7

--- .phan/config.php
+++ .phan/config.php
@@ -1,12 +1,8 @@
 <?php
 
-$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config-library.php';
 
-$cfg['directory_list'] = [
-	'src',
-	'tests',
-	'.phan/stubs',
-];
+$cfg['directory_list'][] = 'tests';
 
 if ( PHP_VERSION_ID >= 80000 && PHP_VERSION_ID < 81000 ) {
 	// Don't complain about 'ReturnTypeWillChange' from PHP 8.1 on 8.0
