bc96eb8cdecd17263e9196413ed48e1d3a7c5dab
James D. Forrester <jforrester@wikimedia.org>
2025-06-05 17:18:46 -0400

    [BREAKING CHANGE] Drop support for PHP < 8.1
    Change-Id: If8ab5510c7cdb4fe8a6948183db7bd4ecc3737ac

--- .phan/config.php
+++ .phan/config.php
@@ -4,14 +4,4 @@ $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config-
 
 $cfg['directory_list'][] = 'tests';
 
-if ( PHP_VERSION_ID >= 80000 && PHP_VERSION_ID < 81000 ) {
-	// Don't complain about 'ReturnTypeWillChange' from PHP 8.1 on 8.0
-	$cfg['suppress_issue_types'] = array_merge(
-		$cfg['suppress_issue_types'],
-		[
-			'PhanUndeclaredClassAttribute',
-		]
-	);
-}
-
 return $cfg;
