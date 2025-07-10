wikimedia/wikipeg

commit 58ac58c86d2273c515ef4b6cf313ecb7c05a34dc

More aggressively throw away results during 'discard' mode
In particular, try hard not to hold onto strings and not to create
new substrings of the input if we don't need to.  Although matches
against literals are likely to be references to intern'ed strings,
computed substring results likely result in needless string
allocation, which can be quickly de-ref'ed in places where we
can't avoid the substring altogether.

This patch requires a major version bump, since a new helper method is
added to the PHP runtime's PEGParserBase.php.

This is a modest ~1% improvement in tokenization time on
[[en:Barack_Obama]] using Parsoid's wikipeg PHP tokenizer.

Change-Id: Ic3a05682ef1cc4157e5f0dfafcbf6a76189ae37f

lib/compiler/language/php.js
@@ -43,6 +43,7 @@ let php = {
   maxFailPos: '$this->maxFailPos',
   assertionSuccess: 'false',
   inputLength: '$this->inputLength',
+  advanceInputChar: 'self::advanceChar($this->input, $this->currPos);',
   consumeInputChar: 'self::consumeChar($this->input, $this->currPos);',
   result: '$result',
   actionArgPrefix: '$',
@@ -246,7 +247,7 @@ let php = {
       + ']';
   },
 
-  matchLiteral(node, reg, result) {
+  matchLiteral(node, reg, result, discard) {
     let literalLength = getUtf8Length(node.value);
     let escapedValue = php.stringify(node.value);
 
@@ -255,7 +256,7 @@ let php = {
       result.condition = `($this->input[$this->currPos] ?? null) === ${escapedValue}`;
       result.onSuccess([
         `$this->currPos++;`,
-        `${reg} = ${php.stringify(node.value)};`
+        `${reg} = ${discard ? 'true' : php.stringify(node.value)};`
       ]);
       return;
     }
@@ -279,12 +280,14 @@ let php = {
       ].join(', ') + ') === 0';
       if (node.ignoreCase) {
         result.onSuccess([
-          `${reg} = substr($this->input, $this->currPos, ${literalLength});`,
+          discard ?
+            `${reg} = true;` :
+            `${reg} = substr($this->input, $this->currPos, ${literalLength});`,
           `$this->currPos += ${literalLength};`
         ]);
       } else {
         result.onSuccess([
-          `${reg} = ${php.stringify(node.value)};`,
+          `${reg} = ${discard ? 'true' : php.stringify(node.value)};`,
           `$this->currPos += ${literalLength};`
         ]);
       }
@@ -340,12 +343,12 @@ let php = {
     return { hasRanges: hasRanges, hasNonAscii: hasNonAscii, chars: chars };
   },
 
-  matchRepeatedClass(node, reg, result, atLeastOne, discarded) {
+  matchRepeatedClass(node, reg, result, atLeastOne, discard) {
     if (node.parts.length === 0) {
       if (node.inverted) {
         // Same as .* / .+
         result.condition = atLeastOne ? '$this->currPos < $this->inputLength' : 'true';
-        if (!discarded) {
+        if (!discard) {
           result.onSuccess([`${reg} = mb_str_split(substr($this->input, $this->currPos), 1, 'utf-8');`]);
         }
         result.onSuccess([`$this->currPos = $this->inputLength;`]);
@@ -369,7 +372,7 @@ let php = {
         result.block.push(`${reg} = strspn($this->input, ${php.stringify(chars.join(''))}, $this->currPos);`);
       }
       result.condition = atLeastOne ? `${reg} > 0` : "true";
-      if (discarded) {
+      if (discard) {
         result.onSuccess([`$this->currPos += ${reg};`]);
       } else {
         // Note that on PHP <= 8.1, str_split('') returns [''] not [], so only
@@ -394,7 +397,10 @@ let php = {
     result.block.push(`${reg} = null;`);
     result.condition = `preg_match(${php.stringify(regexp)}, $this->input, ${reg}, 0, $this->currPos)`;
     result.onSuccess([`$this->currPos += strlen(${reg}[0]);`]);
-    if (!discarded) {
+    if (discard) {
+      // free the match result array
+      result.onSuccess([`${reg} = true;`]);
+    } else {
       // See above: str_split() is only safe to use if at least one match.
       if (hasNonAscii || node.inverted || (!atLeastOne)) {
         result.onSuccess([`${reg} = mb_str_split(${reg}[0], 1, "utf-8");`]);
@@ -404,15 +410,21 @@ let php = {
     }
   },
 
-  matchClass(node, reg, result) {
-    let parts = node.parts;
+  matchClass(node, reg, result, discard) {
 
     // Empty class
     if (node.parts.length === 0) {
       if (node.inverted) {
         // Same as .
         result.condition = '$this->currPos < $this->inputLength';
-        result.onSuccess([`${reg} = self::consumeChar($this->input, $this->currPos);`]);
+        if (discard) {
+          result.onSuccess([
+            `self::advanceChar($this->input, $this->currPos);`,
+            `${reg} = true;`
+          ]);
+        } else {
+          result.onSuccess([`${reg} = self::consumeChar($this->input, $this->currPos);`]);
+        }
       } else {
         // Always fail
         result.condition = 'false';
@@ -424,7 +436,7 @@ let php = {
 
     // Character lists can be done by getting the next character and comparing
     // it sequentially or looking up in a hashtable
-    if (!hasRanges && (hasNonAscii || parts.length <= 2 || php.config.preferClassHashtable)) {
+    if (!hasRanges && (node.parts.length <= 2 || php.config.preferClassHashtable)) {
       if (hasNonAscii || node.inverted) {
         result.block = [`${reg} = self::charAt($this->input, $this->currPos);`];
       } else {
@@ -455,35 +467,55 @@ let php = {
     }
 
     // ASCII character lists can be done with strspn/strcspn
-    if (!hasRanges) {
+    if (!(hasRanges || hasNonAscii)) {
       if (node.inverted) {
         result.condition = `strcspn($this->input, ${php.stringify(chars.join(''))}, `
           + '$this->currPos, 1) !== 0';
-        result.onSuccess([`${reg} = self::consumeChar($this->input, $this->currPos);`]);
+        if (discard) {
+          result.onSuccess([
+            `self::advanceChar($this->input, $this->currPos);`,
+            `${reg} = true;`,
+          ]);
+        } else {
+          result.onSuccess([`${reg} = self::consumeChar($this->input, $this->currPos);`]);
+        }
       } else {
         result.condition = `strspn($this->input, ${php.stringify(chars.join(''))}, `
           + '$this->currPos, 1) !== 0';
-        result.onSuccess([`${reg} = $this->input[$this->currPos++];`]);
+        if (discard) {
+          result.onSuccess([
+            `$this->currPos++;`,
+            `${reg} = true;`,
+          ]);
+        } else {
+          result.onSuccess([`${reg} = $this->input[$this->currPos++];`]);
+        }
       }
       return;
     }
 
     // Otherwise we shall construct a regex
-    if (node.inverted || hasNonAscii) {
-      result.block = [`${reg} = self::charAt($this->input, $this->currPos);`];
-    } else {
-      result.block = [`${reg} = $this->input[$this->currPos] ?? '';`];
-    }
-    let regexp = '/^'
+    let regexp = '/\\A'
       + php.classToRegexp(node)
       + '/'
       + (node.ignoreCase ? 'i' : '')
       + (hasNonAscii ? 'u' : '');
 
-    result.condition = `preg_match(${php.stringify(regexp)}, ${reg})`;
     if (node.inverted || hasNonAscii) {
-      result.onSuccess([`$this->currPos += strlen(${reg});`]);
+      // A multibyte result is possible, and the exact length isn't known
+      // unless/until the match succeeds.  By using preg_match with an offset,
+      // we can avoid creating the substring in the case where the match fails.
+      result.condition = `preg_match(${php.stringify(regexp)}, $this->input, ${reg}, 0, $this->currPos)`;
+      result.onSuccess([
+        `${reg} = ${reg}[0];`,
+        `$this->currPos += strlen(${reg});`
+      ]);
     } else {
+      // Creating the matches array is expensive, and its always done if we
+      // pass an offset to preg_match.  So it's cheaper to do a substring
+      // first, even if we're in 'discard' mode.
+      result.block = [`${reg} = $this->input[$this->currPos] ?? '';`];
+      result.condition = `preg_match(${php.stringify(regexp)}, ${reg})`;
       result.onSuccess(['$this->currPos++;']);
     }
   },

lib/compiler/language/javascript.js
@@ -10,6 +10,7 @@ let javascript = {
   maxFailPos: 'peg$maxFailPos',
   inputLength: 'input.length',
   assertionSuccess: 'void 0',
+  advanceInputChar: 'peg$currPos++',
   consumeInputChar: 'input.charAt(peg$currPos++)',
   result: 'peg$result',
   actionArgPrefix: '',
@@ -176,13 +177,13 @@ let javascript = {
       + ']';
   },
 
-  matchClass(node, reg, result) {
-    let regexp;
+  matchClass(node, reg, result, discard) {
+    let regexp, expr;
     if (node.parts.length === 0) {
       if (node.inverted) {
         // Same as .
         result.condition = 'peg$currPos < input.length';
-        result.onSuccess([`${reg} = input.charAt(peg$currPos++);`]);
+        result.onSuccess([discard ? `${reg} = true;` : `${reg} = input.charAt(peg$currPos++);`]);
       } else {
         // Always fail
         result.condition = 'false';
@@ -192,18 +193,24 @@ let javascript = {
     regexp = '/^'
       + javascript.classToRegexp(node)
       + '/' + (node.ignoreCase ? 'i' : '');
-    result.block = [`${reg} = input.charAt(peg$currPos);`];
-    result.condition = `${regexp}.test(${reg})`;
+    expr = `input.charAt(peg$currPos)`;
+    if (discard) {
+      result.condition = `${regexp}.test(${expr})`;
+      result.onSuccess([`${reg} = true;`]);
+    } else {
+      result.block = [`${reg} = ${expr};`];
+      result.condition = `${regexp}.test(${reg})`;
+    }
     result.onSuccess(['peg$currPos++;']);
   },
 
-  matchRepeatedClass(node, reg, result, atLeastOne, discarded) {
+  matchRepeatedClass(node, reg, result, atLeastOne, discard) {
     let regexp;
     if (node.parts.length === 0) {
       if (node.inverted) {
         // Same as .* / .+
         result.condition = atLeastOne ? 'peg$currPos < input.length' : 'true';
-        if (!discarded) {
+        if (!discard) {
           result.onSuccess([`${reg} = Array.from(input.substring(peg$currPos));`]);
         }
         result.onSuccess([`peg$currPos = input.length;`]);
@@ -222,7 +229,7 @@ let javascript = {
         + (atLeastOne ? '+' : '*') + '/y' + (node.ignoreCase ? 'i' : '');
     result.block.push(`${reg} = ${regexp};`);
     result.block.push(`${reg}.lastIndex = peg$currPos;`);
-    if (discarded) {
+    if (discard) {
       result.condition = `${reg}.exec(input) !== null`;
       result.onSuccess([`peg$currPos = ${reg}.lastIndex;`]);
     } else {
@@ -235,23 +242,34 @@ let javascript = {
     }
   },
 
-  matchLiteral(node, reg, result) {
+  matchLiteral(node, reg, result, discard) {
+    let expr;
     if (node.value.length === 1 && !node.ignoreCase) {
       result.condition = 'input.charCodeAt(peg$currPos) === ' + node.value.charCodeAt(0);
-      result.onSuccess([[reg, ' = ', javascript.stringify(node.value), ';'].join('')]);
+      if (discard) {
+        result.onSuccess([`${reg} = true;`]);
+      } else {
+        result.onSuccess([[reg, ' = ', javascript.stringify(node.value), ';'].join('')]);
+      }
     } else {
       if (node.value.length === 1) {
-        result.block.push([reg, ' = input.charAt(peg$currPos);'].join(''));
+        expr = `input.charAt(peg$currPos)`;
       } else {
-        result.block.push([reg, ' = ',
-          'input.substr(peg$currPos,', node.value.length, ');'].join(''));
+        expr = `input.substr(peg$currPos,${node.value.length})`;
+      }
+      if (!discard) {
+        result.block.push([`${reg} = ${expr};`]);
+        expr = reg;
       }
       if (node.ignoreCase) {
-        result.condition = [reg, '.toLowerCase() === ',
+        result.condition = [expr, '.toLowerCase() === ',
           javascript.stringify(node.value.toLowerCase())].join('');
       } else {
-        result.condition = [reg, ' === ',
+        result.condition = [expr, ' === ',
           javascript.stringify(node.value)].join('');
+      }
+      if (discard) {
+        result.onSuccess([`${reg} = true;`]);
       }
     }
     result.onSuccess([['peg$currPos += ', node.value.length, ';'].join('')]);

lib/compiler/passes/ast-to-code.js
@@ -1222,6 +1222,10 @@ function generateJavascript(ast, options) {
     },
 
     text: function(node, context) {
+      if (context.getDiscard()) {
+        // text does nothing if we're already discarding
+        return recurse(node.expression, context.cloneEnv());
+      }
       var startPos = allocPosReg();
       var result = new Result();
       var reg = context.getResultReg(result);
@@ -1383,13 +1387,13 @@ function generateJavascript(ast, options) {
       var result = new Result();
       // Special case: empty string always matches
       if (node.value.length === 0) {
-        result.expression = "''";
+        result.expression = context.getDiscard() ? "true" : "''";
         result.condition = 'true';
         return result;
       }
 
       var reg = context.getResultReg(result);
-      language.matchLiteral(node, reg, result);
+      language.matchLiteral(node, reg, result, context.getDiscard());
       if (context.getSilence() !== 'true') {
         result.onFailure([
           makeFailCall({
@@ -1405,7 +1409,7 @@ function generateJavascript(ast, options) {
     class: function(node, context) {
       var result = new Result();
       var reg = context.getResultReg(result);
-      language.matchClass(node, reg, result);
+      language.matchClass(node, reg, result, context.getDiscard());
       result.onFailure([`${reg} = ${language.failed};`]);
       if (context.getSilence() !== 'true') {
         result.onFailure([makeFailCall({
@@ -1421,7 +1425,12 @@ function generateJavascript(ast, options) {
       var result = new Result();
       var reg = context.getResultReg(result);
       result.condition = `${language.currPos} < ${language.inputLength}`;
-      result.onSuccess([`${reg} = ${language.consumeInputChar};`]);
+      result.onSuccess(context.getDiscard() ? [
+        `${language.advanceInputChar};`,
+        `${reg} = true;`,
+      ]: [
+        `${reg} = ${language.consumeInputChar};`,
+      ]);
       result.onFailure([`${reg} = ${language.failed};`]);
       if (context.getSilence() !== 'true') {
         result.onFailure([makeFailCall({

src/PEGParserBase.php
@@ -148,6 +148,18 @@ abstract class PEGParserBase {
 		return $char;
 	}
 
+	public static function advanceChar( string $s, int &$byteOffset ): void {
+		if ( !isset( $s[$byteOffset] ) ) {
+			return;
+		}
+		$byteOffset += match ( ord( $s[$byteOffset] ) & 0xf0 ) {
+			default => 1,
+			0xc0, 0xd0 => 2,
+			0xe0 => 3,
+			0xf0 => 4,
+		};
+	}
+
 	/**
 	 * @param mixed $value
 	 * @return mixed

commit 442ad2759212643a1088ed981c021e0254b9d6a3

Bug fix to PHP character class match with offset
This was a simple error introduced in
58ac58c86d2273c515ef4b6cf313ecb7c05a34dc caused by my confusing
the regexp *metacharacter* \A with the regexp *modifier* A.

Caught (multiple times over) by Parsoid test suite after regenerating
the Parsoid tokenizer with the latest PEG prerelease.  Added a wikipeg
test case to hopefully prevent this regression in the future.

Change-Id: Ice123c501f66a6f0ef8c7e08bdeb795e7b88388c

lib/compiler/language/php.js
@@ -495,9 +495,9 @@ let php = {
     }
 
     // Otherwise we shall construct a regex
-    let regexp = '/\\A'
+    let regexp = '/'
       + php.classToRegexp(node)
-      + '/'
+      + '/A'
       + (node.ignoreCase ? 'i' : '')
       + (hasNonAscii ? 'u' : '');
 